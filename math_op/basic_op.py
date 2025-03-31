ca0176c3a8a77312ee507e16e001841570afea0e
suouwang <khaoswang1016@gmail.com>
2025-03-31 15:05:57 +0800

    完成basic_op.py

---
+++ math_op/basic_op.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+
+def substract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def devide(a,b):
+    if b==0:
+        return ("ZeroDivisionError")
+    else:
+        return a/b
